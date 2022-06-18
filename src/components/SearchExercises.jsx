601039f45c305120fcb3198f8fa8e3f5b0538f2c
anandbaraik <anandbaraik2014@gmail.com>
2022-06-18 23:52:58 +0530

    update:search & api, util :art:

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,10 +1,22 @@
 import React, {useEffect, useState} from 'react'
 import { Box, Stack, Button, Typography, TextField } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 const SearchExercises = () => {
 	const [search, setSearch] = useState('');
+	const [exercises, setExercises] = useState([]);
 	const handleSearch = async () => {
 		if(search) {
-			
+			const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+			const searchedExercises = exerciseData?.filter((exercise) =>
+				exercise?.name.toLowerCase().includes(search) ||
+				exercise?.bodyPart.toLowerCase().includes(search) ||
+				exercise?.equipment.toLowerCase().includes(search) ||
+				exercise?.target.toLowerCase().includes(search)
+			)
+			console.log(exerciseData);
+			console.log(searchedExercises);
+			setSearch('');
+			setExercises(searchedExercises);
 		}
 	}
   return (
