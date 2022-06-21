70d9899b72a0aa697de068fdbcb005c0ffca8a4a
anandbaraik <anandbaraik2014@gmail.com>
2022-06-21 23:22:14 +0530

    update:started working on exercise card :art:

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React, {useEffect, useState} from 'react'
+import { Pagination, Box, Stack, Typography } from '@mui/material';
+import {exerciseOptions, fetchData } from "../utils/fetchData";
 const Exercises = ({bodyPart, exercises, setExercises}) => {
+	console.log(exercises);
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+		sx={{
+			mt:{
+				lg:"110px",
+			}
+		}}
+		mt="50px"
+		p="20px">
+		<Typography
+			variant='h3' mb="46px">
+			Showing Results
+		</Typography>
+		<Stack
+			direction="row"
+			sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap"
+			justifyContent="center">
+			
+		</Stack>
+    </Box>
   )
 }
 
