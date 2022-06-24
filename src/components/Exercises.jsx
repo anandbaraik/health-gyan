71a07fb1717e3af1d7842b8004613ede429eb480
anandbaraik <anandbaraik2014@gmail.com>
2022-06-24 23:48:31 +0530

    update:no result found ui

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { Pagination, Box, Stack, Typography } from '@mui/material';
 import {exerciseOptions, fetchData } from "../utils/fetchData";
 import {ExerciseCard} from "../components/ExerciseCard"
+import NotFound from './NotFound';
 const Exercises = ({bodyPart, exercises, setExercises}) => {
 
 	useEffect(() => {
@@ -56,7 +57,7 @@ const Exercises = ({bodyPart, exercises, setExercises}) => {
 					/>
 				))
 				) : (
-					'No exercise'
+					<NotFound/>
 				)
 				
 			}
---
+++ src/components/NotFound.js
@@ -0,0 +1,31 @@
+import { Typography } from '@mui/material';
+import React from 'react'
+import mindFulnessImg from "../assets/images/mindfulness.png";
+const NotFound = () => {
+  return (
+    <div>
+        <img src={mindFulnessImg}
+            alt="mindfulness"
+            style={{
+                width:"100%",
+                maxWidth: "100%"
+            }}
+        />
+        <Typography
+			variant='h3' mt="10px"
+            sx={{
+                textAlign:"center",
+                fontSize:{
+                    lg:'1.9rem',
+                    xs:'1.3rem'
+                }
+            }}
+            className="no-result-found">
+            We didn't find any results, Sorry! <br/>
+            Search similar term!
+		</Typography>
+    </div>
+  )
+}
+
+export default NotFound
