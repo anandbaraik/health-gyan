7154206b4e5c35b3b61b2530b62ea6094fc37bbf
anandbaraik <anandbaraik2014@gmail.com>
2022-06-25 21:38:06 +0530

    update:started working on exercise details

---
+++ src/components/SimillarExercises.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SimillarExercises = () => {
+  return (
+    <div>SimillarExercises</div>
+  )
+}
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {Details} from "../components/Details";
+import {ExerciseVideos} from "../components/ExerciseVideos";
+import {SimillarExercises} from "../components/SimillarExercises";
 const ExerciseDetail = () => {
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Details/>
+      <ExerciseVideos/>
+      <SimillarExercises/>
+    </Box>
   )
 }
 
