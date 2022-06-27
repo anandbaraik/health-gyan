ba2b93f92fbead4b7fa0d0f3e7223cf05489c1cb
anandbaraik <anandbaraik2014@gmail.com>
2022-06-27 23:59:24 +0530

    update:exercise videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,7 +1,89 @@
+import { Box, Stack, Typography } from '@mui/material';
 import React from 'react'
 
-export const ExerciseVideos = () => {
+export const ExerciseVideos = ({exerciseVideos, name}) => {
+  console.log(exerciseVideos);
+  console.log(name);
+  if(!exerciseVideos) return 'Loading';
   return (
-    <div>ExerciseVideos</div>
+    <Box
+    	sx={{
+			marginTop:{
+				lg:"200px",
+				xs:"20px"
+			}
+		}} p="20px">
+		<Typography
+			variant='h3'
+			mb="3px">
+			Watch {` `}
+			<span title={name} style={{color:"#ff2625"}}>
+				{
+					name?.length > 20 ?
+						`${name?.substring(0,20)}...`
+					: name
+				}
+			</span>
+			{` `}exercise videos
+		</Typography>
+		<Stack
+			justifyContent="flex-start"
+			flexWrap="wrap"
+			alignItems="center"
+			sx={{
+				flexDirection:{
+					lg:"row"
+				},
+				gap:{
+					lg:"110px",
+					xs:"0px"
+				}
+			}}>
+			{
+				exerciseVideos?.map((item, index) => (
+					item?.video && (
+						<a
+							key={index}
+							className="exercise-video"
+							href={`https://www.youtube.com/watch?v=${item?.video?.videoId}`}
+							target="_blank"
+							rel="noreferrer">
+							<img
+								alt={item?.video?.title}
+								style={{
+									borderRadius:"20px",
+									width:"360px",
+									height:"202px",
+									objectFit:"cover"
+								}}
+								src={item?.video?.thumbnails?.[0]?.url}
+							/>
+							<Box>
+								<Typography
+									sx={{ fontSize: { lg: '28px', xs: '18px' } }} 
+									fontWeight={600} color="#000"
+									title={item?.video?.title}>
+									{
+										item?.video?.title?.length > 15 ?
+											`${item?.video?.title?.substring(0,15)}...`
+										: name
+									}
+								</Typography>
+								<Typography fontSize="14px" color="#000"
+									title={item?.video?.channelName}>
+									{
+										item?.video?.channelName?.length > 15 ?
+											`${item?.video?.channelName?.substring(0,15)}...`
+										: name
+									}
+								</Typography>
+							</Box>
+						</a>
+					)
+				))
+			}
+		</Stack>
+
+    </Box>
   )
 }
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -4,19 +4,23 @@ import { Box } from '@mui/material'
 import {Details} from "../components/Details";
 import {ExerciseVideos} from "../components/ExerciseVideos";
 import {SimillarExercises} from "../components/SimillarExercises";
-import {fetchData, exerciseOptions} from "../utils/fetchData";
+import {fetchData, exerciseOptions, youTubeOptions} from "../utils/fetchData";
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const {id} = useParams();
 
   useEffect(() => {
 	window.scrollTo({ top: 0, behavior: 'smooth' });
 	const fetchExerciseData = async () => {
 		const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
-      	const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+    const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
 		const exerciseData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions);
 		setExerciseDetail(exerciseData);
+
+    const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetail?.name} exercise`,youTubeOptions);
+		setExerciseVideos(exerciseVideosData.contents);
 	}
 	fetchExerciseData();
   }, [id]);
@@ -28,7 +32,10 @@ const ExerciseDetail = () => {
       <Details
 	  	exerciseDetail={exerciseDetail}
       />
-      <ExerciseVideos/>
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail?.name}
+      />
       <SimillarExercises/>
     </Box>
   )
